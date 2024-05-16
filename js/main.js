e3af2dbf976ba24e2c15fd15827c8450d0736610
Javier Medina <javi86medina@hotmail.com>
2024-05-16 17:17:06 +0200

    Init commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const triggerMenu = document.getElementById('triggerMenu');
+const mainNav = document.getElementById('mainNav');
+
+triggerMenu.addEventListener('click', function() {
+    triggerMenu.classList.toggle('toggled')
+    mainNav.classList.toggle('active')
+});
